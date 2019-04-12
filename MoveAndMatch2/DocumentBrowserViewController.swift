596928045420c75a3b586f4e609779273fb89f4a
Sweeper777 <sumulang@gmail.com>
2019-04-12 08:38:05 +0100

    Update DocumentBrowserViewController.swift

--- MoveAndMatch2/DocumentBrowserViewController.swift
+++ MoveAndMatch2/DocumentBrowserViewController.swift
@@ -32,27 +32,44 @@ class DocumentBrowserViewController: UIDocumentBrowserViewController, UIDocument
     // MARK: UIDocumentBrowserViewControllerDelegate
     
     func documentBrowser(_ controller: UIDocumentBrowserViewController, didRequestDocumentCreationWithHandler importHandler: @escaping (URL?, UIDocumentBrowserViewController.ImportMode) -> Void) {
-        let newDocumentURL: URL? = nil
-        
-        // Set the URL for the new document here. Optionally, you can present a template chooser before calling the importHandler.
-        // Make sure the importHandler is always called, even if the user cancels the creation request.
-        if newDocumentURL != nil {
-            importHandler(newDocumentURL, .move)
-        } else {
+        //1.
+        guard let url = getNewDocumentURL() else {
             importHandler(nil, .none)
+            return
+        }
+        let newdoc = ProjectDocument(fileURL: url)
+        
+        //2.
+        newdoc.save(to: url, for: .forCreating) { saveSuccess in
+            
+            //3.
+            guard saveSuccess else {
+                importHandler(nil, .none)
+                return
+            }
+            
+            //4.
+            newdoc.close { (closeSuccess) in
+                guard closeSuccess else {
+                    importHandler(nil, .none)
+                    return
+                }
+                
+                importHandler(url, .move)
+            }
         }
     }
     
     func documentBrowser(_ controller: UIDocumentBrowserViewController, didPickDocumentsAt documentURLs: [URL]) {
         guard let sourceURL = documentURLs.first else { return }
         
-        // Present the Document View Controller for the first document that was picked.
+        // Present the ProjectDocument View Controller for the first document that was picked.
         // If you support picking multiple items, make sure you handle them all.
         presentDocument(at: sourceURL)
     }
     
     func documentBrowser(_ controller: UIDocumentBrowserViewController, didImportDocumentAt sourceURL: URL, toDestinationURL destinationURL: URL) {
-        // Present the Document View Controller for the new newly created document
+        // Present the ProjectDocument View Controller for the new newly created document
         presentDocument(at: destinationURL)
     }
     
@@ -60,15 +77,27 @@ class DocumentBrowserViewController: UIDocumentBrowserViewController, UIDocument
         // Make sure to handle the failed import appropriately, e.g., by presenting an error message to the user.
     }
     
-    // MARK: Document Presentation
+    // MARK: ProjectDocument Presentation
     
     func presentDocument(at documentURL: URL) {
         
         let storyBoard = UIStoryboard(name: "Main", bundle: nil)
         let documentViewController = storyBoard.instantiateViewController(withIdentifier: "DocumentViewController") as! DocumentViewController
-        documentViewController.document = Document(fileURL: documentURL)
+        documentViewController.document = ProjectDocument(fileURL: documentURL)
         
         present(documentViewController, animated: true, completion: nil)
+    }
+    
+    func getNewDocumentURL() -> URL? {
+        guard let firstPart = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
+            return nil
+        }
+        
+        var numberToTry = 1
+        while FileManager.default.fileExists(atPath: firstPart.appendingPathComponent("Untitled Project \(numberToTry)").appendingPathExtension(ProjectDocument.fileExtension).absoluteString) {
+            numberToTry += 1
+        }
+        return firstPart.appendingPathComponent("Untitled Project \(numberToTry)").appendingPathExtension(ProjectDocument.fileExtension)
     }
 }
 
